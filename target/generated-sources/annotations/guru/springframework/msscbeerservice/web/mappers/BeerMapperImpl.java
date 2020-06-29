d055a951d9f643ecb657436e1f6220b9d9732741
eskendir Berhan <eskiew@yahoo.com>
2020-06-29 11:38:04 -0500

    test fix closes #11

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-29T11:08:35-0500",
+    date = "2020-06-29T11:24:33-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
